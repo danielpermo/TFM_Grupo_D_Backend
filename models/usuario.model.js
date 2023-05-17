77b72b036594d457c3681f79759092c1a606db0a
Laura Perez <laurap48@hotmail.com>
2023-05-17 11:42:04 +0200

    Añadir al modelo usuarios getByRol, update y deleteById

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -1,3 +1,7 @@
+//obtener el listado de alumnos o profesores no borrados
+const getByRol = (rol) => {
+    return db.query('SELECT * FROM usuarios WHERE rol=? AND borrado=0', [rol]);
+}
 const getById = (userId) => {
     return db.query('SELECT * FROM usuarios WHERE id=? AND borrado=0', [userId]);
 }
@@ -12,7 +16,19 @@ const create = ({ nombre, apellidos, username, email, password, telefono, direcc
         [nombre, apellidos, username, email, password, telefono, direccion, ciudad, latitud, longitud, edad, fecha_nacimiento, genero, dni, rol]);
 }
 
+//antes de pasar los valores encriptar password
+const update = (usuarioId, { nombre, apellidos, username, email, password, telefono, direccion, ciudad, latitud, longitud, edad, fecha_nacimiento, genero, dni, rol }) => {
+    return db.query(
+        'UPDATE usuarios SET nombre=?, apellidos=?, username=?, email=?, password=?, telefono=?, direccion=?, ciudad=?, latitud=?, longitud=?, edad=?, fecha_nacimiento=?, genero=?, dni=?, rol=? WHERE id=?',
+        [nombre, apellidos, username, email, password, telefono, direccion, ciudad, latitud, longitud, edad, fecha_nacimiento, genero, dni, rol, usuarioId]
+    );
+}
+
+//borrado lógico de usuarios
+const deleteById = (usuarioId) => {
+    return db.query('UPDATE usuarios SET borrado=1 WHERE id=?', [usuarioId]);
+}
 
 module.exports = {
-    create, getById, getByEmail
+    create, getById, getByEmail, getByRol, update, deleteById
 }

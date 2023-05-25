9492ea47e247c14d3b4963baa7e9e2e57ae096a3
Laura Perez <laurap48@hotmail.com>
2023-05-25 09:45:13 +0200

    Función para obtener coordenadas y añadirlo al registro de usuarios

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,6 @@
 const dayjs = require('dayjs');
 const jwt = require('jsonwebtoken');
+const NodeGeocoder = require('node-geocoder');
 
 const createToken = (usuario) => {
     const dataToken = {
@@ -8,8 +9,27 @@ const createToken = (usuario) => {
         exp: dayjs().add(1, 'days').unix()
     }
     return jwt.sign(dataToken, 'TFM Grupo D');
-}
+};
+
+const getCoordenadas = async (direccion, ciudad) => {
+    const options = {
+        provider: 'openstreetmap',
+        city: ciudad,
+        country: 'España',
+        countrycodes: 'es',
+        format: 'json',
+        limit: 1
+    }
+    const direccionCompleta = `${direccion}, ${ciudad}, España`;
+    const geocoder = NodeGeocoder(options);
+    try {
+        const res = await geocoder.geocode(direccionCompleta);
+        return res[0];
+    } catch (error) {
+        return error.message;
+    }
+};
 
 module.exports = {
-    createToken
+    createToken, getCoordenadas
 }

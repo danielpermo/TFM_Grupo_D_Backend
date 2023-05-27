5b484f5e165769d1d68826c16134b98e1577f3ec
Laura Perez <laurap48@hotmail.com>
2023-05-27 20:00:58 +0200

    Añadir al modelo profesores getByUsuarioId y updateValidacion

--- models/profesor.model.js
+++ models/profesor.model.js
@@ -1,7 +1,14 @@
+const getByUsuarioId = (usuarioId) => {
+    return db.query('SELECT * FROM profesores WHERE usuario_id=?', [usuarioId]);
+}
 
-
+const update = (usuarioId, { experiencia, precio, validado }) => {
+    return db.query('UPDATE profesores SET experiencia=?, precio=?, validado=? WHERE usuario_id=?', [experiencia, precio, validado],)
+}
+const updateValidacion = (usuarioId, { validado }) => {
+    return db.query('UPDATE profesores SET validado=? WHERE usuario_id=?', [validado, usuarioId,]);
+}
 
 module.exports = {
-
-    // Nombres de las funciones que se van a exportar
+    getByUsuarioId, update, updateValidacion
 }

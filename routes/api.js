59eabca21926a876e95cad439123ab2de0560d99
Laura Perez <laurap48@hotmail.com>
2023-06-01 14:48:58 +0200

    Añadir middlewares comprobar login y rol y administradores

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
+const { checkToken, checkAdmin } = require('../utils/middlewares');
 
 router.use('/asignaturas', require('./api/asignaturas'));
 router.use('/usuarios', require('./api/usuarios'));
 router.use('/profesores', require('./api/profesores'));
 router.use('/alumnos', require('./api/alumnos'));
-router.use('/administradores', require('./api/administradores'))
+router.use('/administradores', checkToken, checkAdmin, require('./api/administradores'))
 router.use('/clases', require('./api/clases'));
-
 
 module.exports = router;

2ed7a6c4bed656e69fb83c1e39b1173982e7c1b0
Laura Perez <laurap48@hotmail.com>
2023-06-04 18:47:21 +0200

    Añadir rutas para listado alumnos, perfil profesor y actualizar profesor

--- models/profesor.model.js
+++ models/profesor.model.js
@@ -24,8 +24,8 @@ const create = (usuarioId, { experiencia, precio }) => {
     return db.query('INSERT INTO profesores (experiencia, precio, usuario_id) VALUES (?,?,?)', [experiencia, precio, usuarioId]);
 }
 
-const update = (usuarioId, { experiencia, precio, validado }) => {
-    return db.query('UPDATE profesores SET experiencia=?, precio=?, validado=? WHERE usuario_id=?', [experiencia, precio, validado],)
+const update = (usuarioId, { experiencia, precio }) => {
+    return db.query('UPDATE profesores SET experiencia=?, precio=? WHERE usuario_id=?', [experiencia, precio, usuarioId])
 }
 const updateValidacion = (usuarioId, { validado }) => {
     return db.query('UPDATE profesores SET validado=? WHERE usuario_id=?', [validado, usuarioId,]);
